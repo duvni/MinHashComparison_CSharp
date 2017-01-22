784326bcb15ef3e842ffe2253c8051f215332e60
duvni <daniel87@gmail.com>
2017-01-22 14:07:14 +0200

    Core code
    The core code for the MinHash comparison

---
+++ C#/MinHash.cs
@@ -0,0 +1,176 @@
+using System;
+using System.Text;
+
+namespace MinHashComparison
+{
+	public class MinHash
+	{
+		/// <summary>
+		/// Numebr of hash functions
+		/// </summary>
+		private readonly int _numHashFunctions;
+
+		/// <summary>
+		/// Number of tokens in a word (k-shingling)
+		/// </summary>
+		private readonly int _tokensInWord;
+
+		/// <summary>
+		/// Array of hash functions
+		/// </summary>
+		private readonly HashGenerator[] _hashFunctions;
+
+		/// <summary>
+		/// Hash function generator.
+		/// Given a set of hash function parameters (a, b, c) and a bound on possible hash value,
+		/// generates a hash function that given an element x returns its hashed value.
+		/// </summary>
+		private class HashGenerator
+		{
+			private readonly int _a;
+			private readonly int _b;
+			private readonly int _c;
+			private readonly int _universeSize;
+
+			/// <summary>
+			/// Constructor
+			/// </summary>
+			/// <param name="a">Hash function parameter</param>
+			/// <param name="b">Hash function parameter</param>
+			/// <param name="c">Hash function parameter</param>
+			/// <param name="universeSize">Upper bound on hash values - should be a Mersenne prime (e.g. 131071 = 2^17 - 1)</param>
+			public HashGenerator(int a, int b, int c, int universeSize)
+			{
+				_a = a;
+				_b = b;
+				_c = c;
+				_universeSize = universeSize;
+			}
+
+			/// <summary>
+			/// Hash function calculator
+			/// </summary>
+			/// <param name="x">Hash function parameter</param>
+			/// <returns>Hashed value</returns>
+			public int CalculateHash(int x)
+			{
+				// Modify the hash family as per the size of possible elements in a Set
+				x = x & _universeSize;
+				int hashValue = (int)((_a * (x >> 4) + _b * x + _c) & _universeSize);
+				return Math.Abs(hashValue);
+			}
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="tokensInWord">number of tokens for a word</param>
+		/// <param name="numHashFunctions">number of min hash functions to compute</param>
+		public MinHash(int tokensInWord, int numHashFunctions)
+		{
+			if (tokensInWord <= 0)
+			{
+				throw new Exception(String.Format("MinHash - Illegal number of tokens in a word: {0}", tokensInWord));
+			}
+
+			if (numHashFunctions <= 0)
+			{
+				throw new Exception(String.Format("MinHash - Illegal number of hash functions: {0}", numHashFunctions));
+			}
+
+			_tokensInWord = tokensInWord;
+			_numHashFunctions = numHashFunctions;
+
+			// Generate all hash functions
+			_hashFunctions = new HashGenerator[numHashFunctions];
+
+			int universeSize = Int32.MaxValue; // Max Integer (2^31-1) is a Mersenne prime
+			Random r = new Random();
+			for (int i = 0; i < numHashFunctions; i++)
+			{
+				int a = r.Next(universeSize);
+				int b = r.Next(universeSize);
+				int c = r.Next(universeSize);
+				_hashFunctions[i] = new HashGenerator(a, b, c, universeSize);
+			}
+		}
+
+		/// <summary>
+		/// Compute the MinHash Sketch from an array of tokens.
+		/// Update the hash tables according to the min values of the sketch.
+		/// </summary>
+		/// <param name="tokens">A list of tokens</param>
+		/// <returns>An array of minimun hash values</returns>
+		public int[] ComputeSketch(string[] tokens)
+		{
+			// Maintain an array of minimum hash values
+			int[] hashMinimumValues = new int[_numHashFunctions];
+
+			// Since we're looking for minimum values,
+			// it's important to initialize the array to max int
+			for (int i = 0; i < hashMinimumValues.Length; i++)
+			{
+				hashMinimumValues[i] = Int32.MaxValue;
+			}
+
+			if (tokens == null || tokens.Length == 0)
+			{
+				return hashMinimumValues;
+			}
+
+			// Go over all tokens and generate words (k-shingling)
+			for (int tokenIndex = 0; tokenIndex < tokens.Length; tokenIndex++)
+			{
+				// Build the word by concatenating consecutive tokens
+				StringBuilder wordBuilder = new StringBuilder(_tokensInWord);
+				for (int i = tokenIndex; i < tokenIndex + _tokensInWord; i++)
+				{
+					if (i < tokens.Length)
+					{
+						wordBuilder.Append(tokens[i]);
+					}
+					else
+					{
+						break;
+					}
+				}
+
+				int hashCode = wordBuilder.ToString().GetHashCode();
+
+				// Go over all hash functions			
+				for (int hashIndex = 0; hashIndex < _numHashFunctions; hashIndex++)
+				{
+					// compute hash value of token with current hash function
+					int hashValue = _hashFunctions[hashIndex].CalculateHash(hashCode);
+
+					// Update minimum value at index hashIndex
+					hashMinimumValues[hashIndex] = Math.Min(hashMinimumValues[hashIndex], hashValue);
+				}
+			}
+
+			// Return the MinHash Sketch
+			return hashMinimumValues;
+		}
+
+		/// <summary>
+		/// Compares two MinHash sketches
+		/// </summary>
+		/// <param name="firstMinHashSketch">The first MinHash sketch to compare</param>
+		/// <param name="secondMinHashSketch">The second MinHash sketch to compare</param>
+		/// <returns>Similarity result (between 0 and 1)</returns>
+		public double CompareSketches(int[] firstMinHashSketch, int[] secondMinHashSketch)
+		{
+			// count equal hashes
+			int equalHashes = 0;
+			for (int i = 0; i < _numHashFunctions; i++)
+			{
+				if (firstMinHashSketch[i] == secondMinHashSketch[i])
+				{
+					equalHashes++;
+				}
+			}
+
+			return (1.0 * equalHashes) / _numHashFunctions; // similarity index
+		}
+	}
+}
---
+++ C#/MinHashSimilarity.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinHashComparison
+{
+	public class MinHashSimilarity
+	{
+		/// <summary>
+		/// The internal min hash instance
+		/// </summary>
+		private readonly MinHash _minHash;
+
+		/// <summary>
+		/// The threshold in which documents are considered similar
+		/// </summary>
+		private readonly double _threshold;
+
+		/// <summary>
+		/// Number of bands for LSH comparison
+		/// </summary>
+		private readonly int _bands;
+
+		/// <summary>
+		/// number of rows for LSH comparison
+		/// </summary>
+		private readonly int _rows;
+
+		/// <summary>
+		/// Buckets for LSH comparison
+		/// </summary>
+		private readonly Dictionary<string, List<int[]>> _buckets;
+
+		/// <summary>
+		/// Default Constructor
+		/// Works best if threshold is ~90%
+		/// </summary>
+		/// <param name="threshold">The threashold in which documents are considered similar</param>
+		public MinHashSimilarity(double threshold) : this (threshold, 5, 400, 20, 20)
+		{
+			// Likelihood of an LSH match between two documents (1-(1-J(A,B)^rows)^bands) | J(A,B) = Jaccard index, rows = 20, bands = 20
+			// J(A,B)   Probability of getting compared
+			// .7       .016
+			// .8       .206
+			// .85      .546
+			// .861     .642 // sCurve ((1/b)^(1/r))
+			// .87      .720
+			// .9       .925
+			// .95      .999
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="threshold">The threashold in which documents are considered similar</param>
+		/// <param name="tokensInWord">number of tokens for a word</param>
+		/// <param name="numHashFunctions">number of min hash functions to compute</param>
+		/// <param name="bands">number of bands for LSH comparison</param>
+		/// <param name="rows">number of rows for LSH comparison</param>
+		public MinHashSimilarity(double threshold, int tokensInWord, int numHashFunctions, int bands, int rows)
+		{
+			if (threshold < 0 || threshold > 100)
+			{
+				throw new Exception(String.Format("MinHashSimilarity - Illegal threshold: {0}", threshold));
+			}
+			if (bands*rows != numHashFunctions)
+			{
+				throw new Exception("MinHashSimilarity - bands * rows != numHashFunctions");
+			}
+
+			_threshold = threshold;
+			_minHash = new MinHash(tokensInWord, numHashFunctions);
+			_bands = bands;
+			_rows = rows;
+			_buckets = new Dictionary<string, List<int[]>>();
+		}
+
+		/// <summary>
+		/// Clears all history of documents
+		/// </summary>
+		public void ClearDocuments()
+		{
+			_buckets.Clear();
+		}
+
+		/// <summary>
+		/// Given a string document, looks whether a similar document was already seen
+		/// </summary>
+		/// <param name="doc">The new document to compare to</param>
+		/// <returns>true if a similar document was already seen</returns>
+		public bool LookForSimilarDocument(string doc)
+		{
+			int[] minHashes = _minHash.ComputeSketch(doc.Split(new char[]{' ', '\t', '\r', '\n'}));
+			string[] bandHashes = new string[_bands];
+			HashSet<int[]> comparedSketches = new HashSet<int[]>();
+
+			for (int i = 0; i < _bands; i++)
+			{
+				bandHashes[i] = ComputeBandHash(minHashes, i);
+
+				if (_buckets.ContainsKey(bandHashes[i]))
+				{
+					foreach (int[] sketchToCompare in _buckets[bandHashes[i]])
+					{
+						if (!comparedSketches.Contains(sketchToCompare))
+						{
+							if (_minHash.CompareSketches(minHashes, sketchToCompare) >= _threshold)
+							{
+								// Found a similar document
+								return true;
+							}
+
+							// Avoid comparing two documents twice
+							comparedSketches.Add(sketchToCompare);
+						}
+					}
+				}
+			}
+
+			// No match found, add document to buckets
+			for (int i = 0; i < _bands; i++)
+			{
+				if (!_buckets.ContainsKey(bandHashes[i]))
+				{
+					_buckets.Add(bandHashes[i], new List<int[]>());
+				}
+				_buckets[bandHashes[i]].Add(minHashes);
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Computes a hash for quick bucket match search
+		/// </summary>
+		/// <param name="minHashes">The MinHashes for row values</param>
+		/// <param name="i">The ith band</param>
+		/// <returns></returns>
+		private string ComputeBandHash(int[] minHashes, int i)
+		{
+			StringBuilder bandHashSB = new StringBuilder((_rows + 1) * 10);
+			for (int j = 0; j < _rows; j++)
+			{
+				// adding the rows corresponding to ith band
+				bandHashSB.Append(minHashes[i * _rows + j].ToString().PadLeft(10, '0'));
+			}
+			// adding the number i to distinguish between bands
+			bandHashSB.Append(i.ToString().PadLeft(10, '0'));
+			return bandHashSB.ToString();
+		}
+	}
+}
